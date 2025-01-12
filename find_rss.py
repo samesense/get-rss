b770161fad8ea443ed0d78380919b9af9cf9fa51
James-Evans <James-Evans@sparktx.com>
2025-01-12 08:25:45 -0500

    get rss from website

---
+++ find_rss.py
@@ -0,0 +1,22 @@
+from smolagents import CodeAgent, HfApiModel
+
+url = "https://www.nature.com/cgt/"
+url = "https://www.mdpi.com/journal/genes"
+# model_id = "meta-llama/Llama-3.3-70B-Instruct"
+#
+# model = HfApiModel(model_id=model_id, token="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")
+# agent = CodeAgent(tools=[], model=model, add_base_tools=True)
+#
+# agent.run(
+#     "Could you give me the 118th number in the Fibonacci sequence?",
+# )
+
+# model = LiteLLMModel(model_id="ollama_chat/llama3.2", api_key="ollama")
+m = "describeai/gemini-small"
+m = "meta-llama/Llama-3.3-70B-Instruct"
+# m = "microsoft/phi-4"
+model = HfApiModel(model_id=m)
+agent = CodeAgent(
+    tools=[], model=model, additional_authorized_imports=["requests", "bs4"]
+)
+agent.run(f"Could you get me the url of the rss feed of the page at url '{url}'?")
